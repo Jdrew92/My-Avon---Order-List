76927d47a36f93bce2febe645a7ef98a0f6a62a5
Jandres <darkkhir0@gmail.com>
2020-11-24 17:32:55 -0500

    Commit Inicial

---
+++ app/src/main/java/com/example/myavon_orderlist/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.myavon_orderlist;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        FloatingActionButton fab = findViewById(R.id.fabAddCampaign);
+    }
+}
